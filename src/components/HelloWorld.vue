fbc7c3ceea58548698f2ab69efed9ac8c36196a3
haannn123 <haaannn97@gmail.com>
2026-08-17 13:29:05 -0400

    Remove Typescript Error

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
-  import viteLogo from '../assets/vite.svg'
-  import heroImg from '../assets/hero.png'
-  import vueLogo from '../assets/vue.svg'
+
 </script>
 
 <template>
